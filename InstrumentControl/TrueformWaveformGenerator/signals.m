24db481403ddf44d67a56e41b28a65e3d6452863
AndrewMZ <andreika3981@gmail.com>
2022-04-27 18:37:58 +0700

    added stuff

--- InstrumentControl/TrueformWaveformGenerator/signals.m
+++ InstrumentControl/TrueformWaveformGenerator/signals.m
@@ -1,22 +1,55 @@
 classdef signals
-    %SIGNALS Summary of this class goes here
-    %   Detailed explanation goes here
+    % methods:
+    %   signals.two_sins(n, fs, f1, f2)
+    %   create two sinusoids and sum it
     
     properties
-        Property1
+        
     end
     
-    methods
-        function obj = signals(inputArg1,inputArg2)
-            %SIGNALS Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+    methods (Static)
+        function sig_sum = two_sins(n, fs, f1, f2)
+            % временная ось для создания синусоиды
+            timeline = 0:1/fs:(n - 1)/fs;
+            
+            % генерация синусоид
+            sig1 = sin(2*pi*f1*timeline);
+            sig2 = sin(2*pi*f2*timeline);
+            
+            % 1 сигнал, который будет отправлен на ЦАП 
+            sig_sum = real(sig1) + real(sig2);
         end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function [ofdmTime, params] = ofdm(guardSize, fftSize)
+            infoScNum = fftSize - 2*guardSize;
+            infoHalf = infoScNum/2;
+            L1 = guardSize + 1;
+            L2 = guardSize + infoHalf;
+            bits = randi([0, 1], 1, infoScNum*2);
+            moded = (qammod(bits', 4, 'InputType', 'bit'))';
+            spectrum = zeros(1, fftSize);
+            spectrum(L1:L2) = moded(1:infoHalf);
+            R1 = L2 + 2;
+            R2 = R1 -1 + infoHalf;
+            spectrum(R1:R2) = moded(infoHalf + 1:infoScNum);
+            spectrum_shifted = fftshift(spectrum);
+            ofdmTime = ifft(spectrum_shifted);
+            
+            params.guardSize = guardSize;
+            params.fftSize = fftSize;
+            params.bits = bits;
+        end
+        function [demodedBits, err] = ofdm_demod(outerSig, params)
+            ofdmSpecShifted = fft(outerSig);
+            ofdmSpec = fftshift(ofdmSpecShifted);
+            infoScNum = params.fftSize - 2*params.guardSize;
+            infoHalf = infoScNum/2;
+            L1 = params.guardSize + 1;
+            L2 = params.guardSize + infoHalf;
+            R1 = L2 + 2;
+            R2 = R1 -1 + infoHalf;
+            noGuardsAndZeros = [ofdmSpec(L1:L2), ofdmSpec(R1:R2)];
+            demodedBits = (qamdemod(noGuardsAndZeros', 4, 'OutputType', 'bit'))';
+            [~, err] = biterr(params.bits, demodedBits);
         end
     end
 end
